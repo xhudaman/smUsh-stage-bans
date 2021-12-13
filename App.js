8de11080e64899f75f32b7da5fedfd8321dad1cf
xhudaman <edell.matthew@gmail.com>
2021-12-13 03:23:42 -0500

    Change status bar based on selected theme

--- App.js
+++ App.js
@@ -7,26 +7,6 @@ import { View, StyleSheet, useColorScheme } from "react-native";
 
 const Drawer = createDrawerNavigator();
 
-const TheirTheme = {
-  dark: false,
-  colors: {
-    primary: "rgb(255, 45, 85)",
-    background: "rgb(242, 242, 242)",
-    card: "rgb(255, 255, 255)",
-    text: "rgb(28, 28, 30)",
-    border: "rgb(199, 199, 204)",
-    notification: "rgb(255, 69, 58)"
-  }
-};
-
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-    </View>
-  );
-};
-
 const App = () => {
   const scheme = useColorScheme();
 
@@ -41,7 +21,16 @@ const App = () => {
   return (
     <NavigationContainer theme={theme}>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen
+          name="Home"
+          component={({ navigation }) => {
+            return (
+              <View style={styles.container}>
+                <StatusBar style={scheme === "dark" ? "light" : "dark"} />
+              </View>
+            );
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
